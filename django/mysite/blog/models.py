f08c8014c0ab469f5b2861da38bfc0ec8a1a3b39
josisantos12 <josiane15santos24@gmail.com>
2025-03-25 15:08:43 -0300

    models

--- django/mysite/blog/models.py
+++ django/mysite/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)  # Título do post
+    conteudo = models.TextField()  # Conteúdo do post
+    autor = models.CharField(max_length=100)  # Nome do autor
+    data_publicacao = models.DateTimeField(auto_now_add=True)  # Data de publicação automática
+
+    def __str__(self):
+        return self.titulo  # Representação do modelo em string
