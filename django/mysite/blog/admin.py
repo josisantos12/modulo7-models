eeeef56e89d9e07d2d265cdb64e70d381e9c51b4
josisantos12 <josiane15santos24@gmail.com>
2025-03-25 15:34:56 -0300

    django-admin

--- django/mysite/blog/admin.py
+++ django/mysite/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Registrando o modelo Post no Django Admin
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'data_publicacao')  # Exibe estas colunas na interface
+    search_fields = ('titulo', 'autor')  # Adiciona campo de pesquisa por título e autor
+    list_filter = ('data_publicacao',)  # Permite filtrar por data de publicação
